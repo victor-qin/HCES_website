2f10251e3d285e361fc07cc88eee591eba458d2c
kvothey <victor_l_qin@college.harvard.edu>
2018-03-18 09:04:43 -0400

    Added flask application file

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies) - will need to figure out cookies later
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+	return "Hello World"
